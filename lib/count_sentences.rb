cf7663ff6493d7ae0c5b1d54f5e817bf4f9d5df8
Marie Cain <mariecain27@gmail.com>
2018-06-19 02:32:30 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,9 @@ class String
   def count_sentences
     complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
     new_array = complex_string.split(/[!.?]/)
-    new_array.count   
+    new_array.count 
+    no_empty_array = new_array.reject { |i| i.empty? }
+    no_empty_array.count
     binding.pry
   end
 end
